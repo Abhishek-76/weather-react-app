f92c9c32ea0ce3b162326d37edebba2a06b12ed4
Abhishek-76 <avinashgautam8923933406@gmail.com>
2022-12-22 21:10:24 +0530

    2

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import api_keys from "./api_keys";
+import ReactAnimatedWeather from "react-animated-weather/build/ReactAnimatedWeather";
 
 const forecast = (props) => {
   const [query, setQuery] = useState("");
@@ -42,7 +43,81 @@ const forecast = (props) => {
     search("Delhi");
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="forecast">
+      <div className="forecast-icon">
+        <ReactAnimatedWeather
+          icon={props.icon}
+          color={defaults.color}
+          size={defaults.size}
+          animate={defaults.animate}
+        />
+      </div>
+      <div className="today-weather">
+        <h3>{props.weather}</h3>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search city"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+          />
+          <div className="img-box">
+            {" "}
+            <img
+              src="https://images.avishkaar.cc/workflow/newhp/search-white.png"
+              onClick={search}
+            />
+          </div>
+        </div>
+        <ul>
+          {typeof weather.main != "undefined" ? (
+            <div>
+              {" "}
+              <li className="cityHead">
+                <p>
+                  {weather.name}, {weather.sys.country}
+                </p>
+                <img
+                  className="temp"
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                />
+              </li>
+              <li>
+                Temperature{" "}
+                <span className="temp">
+                  {Math.round(weather.main.temp)}°c ({weather.weather[0].main})
+                </span>
+              </li>
+              <li>
+                Humidity{" "}
+                <span className="temp">
+                  {Math.round(weather.main.humidity)}%
+                </span>
+              </li>
+              <li>
+                Visibility{" "}
+                <span className="temp">
+                  {Math.round(weather.visibility)} mi
+                </span>
+              </li>
+              <li>
+                Wind Speed{" "}
+                <span className="temp">
+                  {Math.round(weather.wind.speed)} Km/h
+                </span>
+              </li>
+            </div>
+          ) : (
+            <li>
+              {error.query} {error.message}
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default forecast;
